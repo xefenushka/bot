22997210541f457d9694c156366070834cc9d460
no <no@no.no>
2018-11-08 13:18:26 +0300

    bells

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const TOKEN = process.env.TLGTOKEN
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.onText(/\/test/, (msg, match) => {
-	bot.sendMessage(msg.chat.id , `test success`)
+bot.onText(/\/bells/, (msg, match) => {
+	bot.sendMessage(msg.chat.id , `9.15-9.55
+10.05-10.45
+11.00-11.40
+11.55-12.35
+12.50-13.30
+13.40-14.20
+14.35-15.15
+15.20-16.00`)
 })
 
 bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}`)
+  bot.sendMessage(msg.chat.id, `загрузка... --------------] 99.9% , ${msg.from.first_name}`)
 })
